183ab474f1c24e821760082b973f1d7cfb722d4c
Diego-vizueta <125387881+Diego-vizueta@users.noreply.github.com>
2025-10-16 02:18:28 -0500

    Add files via upload

---
+++ node.js
@@ -0,0 +1,7 @@
+node clean_project.js
+   ```
+3. **Revisa los archivos marcados** con el prefijo `TO_DELETE_` antes de eliminarlos permanentemente.
+
+---
+
+### 🛠️ **Script actualizado para tu proyecto**
